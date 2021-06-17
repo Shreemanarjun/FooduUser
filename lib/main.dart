d79a7e054f82b0d73611c251d8727518b67b31b1
Shreemanarjun <shreemanarjunsahu@gmail.com>
2021-06-17 08:21:24 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ import 'package:path_provider/path_provider.dart';
 
 import 'FooduUserApp.dart';
 
+
+//
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
